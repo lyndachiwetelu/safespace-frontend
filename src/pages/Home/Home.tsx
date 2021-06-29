bb6e11b763999fff080f4e94936f19c0b21ef9f1
Lynda Chiwetelu <lyndachiwe@gmail.com>
2021-06-29 20:11:50 +0200

    Home page UI

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,50 @@
 import FullLayout from "../../components/Layout/FullLayout"
 import './Home.css'
 import banner from '../../images/banner.png'
+import { Button, Col, Row } from "antd"
+import { Redirect } from "react-router-dom"
+import imgChecked from '../../images/checked.png'
+import imgConversation from '../../images/conversation.png'
+import imgStar from '../../images/star.png'
+import { useState } from "react"
 
 const Home = () => {
+    const [redirect, setRedirect] = useState(<></>)
+    const gotoQuestionnaire = () => {
+        setRedirect(<Redirect to="/get-started" />)
+    }
+
     return (
     <FullLayout>
+        {redirect}
         <div className='HomeContent'>
-            <img src={banner} alt="banner" />
+        <Row>
+            <Col lg={12} xs={20} className="HomeContent__Col__Heading">
+                <h1 className="HomeContent__Heading">FIND THE PERFECT THERAPIST ONLINE</h1>
+                <p>Just answer a few questions about yourself and we will find you the perfect therapist. Online and Affordable!</p>
+                <Row className="Statistics">
+                <Col lg={2} xs={2}></Col>
+                <Col lg={18} xs={18}>
+                     <Button className="HomeContent__Col__Heading__Button" size="large" onClick={() => gotoQuestionnaire()}>GET STARTED</Button>
+                </Col>
+                </Row>
+                </Col>
+            <Col lg={12} xs={20} className="HomeContent__Col__Image"><img src={banner} alt="banner" className="HomeContent__Image"/></Col>
+        </Row>
+        <Row className="Statistics">
+            <Col lg={8} xs={8}>
+                <img src={imgChecked} alt='checked' /> 
+                <h1>12,000 Completed Sessions</h1>
+            </Col>
+            <Col lg={8} xs={8}>
+                <img src={imgConversation} alt='active sessions' /> 
+                <h1>1200 Active Sessions</h1>
+            </Col>
+            <Col xs={8}>
+                <img src={imgStar} alt='stars' /> 
+                <h1>500 5 Star Reviews</h1>
+            </Col>
+        </Row>
         </div>
     </FullLayout>
     )
